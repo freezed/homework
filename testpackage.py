8ac4b13cc3a0cf316dec5192ba5092c083192a99
Fred Z <freezed@users.noreply.github.com>
2018-02-06 11:29:42 +0100

    Mise en conformite convention nommage & description: testpackage.py

--- testpackage.py
+++ testpackage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# 1.7-test_package.py : essai d'utilisation d'un package
+"""
+Cours OC/python 3 - Modularite
+[Ex 1.7: testpackage](https://openclassroom/courses/apprenez-a-programmer-en-python/pas-a-pas-vers-la-modularite-2-2)
 
-# OpenClassrooms - Apprenez à programmer en Python - Modularité
-# https://openclassroom/courses/apprenez-a-programmer-en-python/pas-a-pas-vers-la-modularite-2-2
+essai d'utilisation d'un package
+"""
 
 from package.fonctions import table
 
