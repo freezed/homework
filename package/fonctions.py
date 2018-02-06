4a4fb39bdec446c1c6b17f3df28b060dcff226a2
Fred Z <freezed@users.noreply.github.com>
2018-02-06 11:28:14 +0100

    Mise en conformite convention nommage & description: afficherflottant.py

--- package/fonctions.py
+++ package/fonctions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# fonctions.py : essai d'utilisation d'un package
+"""
+Cours OC/python 3
 
-# OpenClassrooms - Apprenez à programmer en Python - Modularité
-# https://openclassroom/courses/apprenez-a-programmer-en-python/pas-a-pas-vers-la-modularite-2-2
+Module de test: essai d'utilisation d'un package
 
-"""Module de test, contient les fonctions crees durant le cours
-OpenClassrooms - Apprenez à programmer en Python"""
+Utilise par:
+- ../afficherflottant.py
+
+"""
 
 
 def table(n, m=11):
