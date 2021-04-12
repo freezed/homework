165d391a4a4a9825ca7492a064c07e7a3251b6c0
freezed <2160318-free_zed@users.noreply.gitlab.com>
2021-04-13 00:57:40 +0200

    📝 Explain script purpose in docstings
    forga/process/fr/embarquement#6

---
+++ tuto-pysdur/pgcd.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+"""
+Author: freezed <2160318-free_zed@users.noreply.gitlab.com> 2021-04-13
+Version: 0.1
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+This is a script following
+https://gitlab.com/forga/process/fr/embarquement/-/issues/6
+
+The goal is to build durable python script using standard library
+"""
+
+
+def pgcd(a,b):
+    """
+    This function find the Greatest Common Divisor (PGCD in French)
+    between a & b
+    """
+    pass
